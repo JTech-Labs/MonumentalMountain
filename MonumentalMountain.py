c0a7a304e749870f6852e437f1edf20dfeedc4bf
JTech-Labs <jteamsuperteam@hotmail.com>
2023-06-09 13:05:54 +0100

    Some Changes

--- MonumentalMountain.py
+++ MonumentalMountain.py
@@ -104,9 +104,11 @@ if dir1 == "left":
                 print_slow("Even though you see this unknown animal very frightening, you decided to gently hush in order to calm it down.")
                 print_slow("The cockroach looks at you with confusion. To your surprise it responds and says 'What are you doing? I am the guardian of the Isoriac dessert. My unlucky body normally frightens most people but I'm actually very friendly!")
                 print_slow("You discover that its name in Inorilarn and that he has been here since the first ray of light dried the sandy land. He hasn't talked to anyone in a lot of time as most adventurers are too terrified. It gives you a golden necklace as a reward and an orange orb that will help you incase you need his help.")
+                print_slow("(This only  has three uses)")
                 print_slow("After that, you decide to go back and decide that the rocks might have something useful.")
                 inventory = inventory + "Inorilarn's-Orb_Light-Necklace_"
                 power = power + 3
+                ororb = int(1)
             else:
                 print_slow("You have already been here.")
         else:
